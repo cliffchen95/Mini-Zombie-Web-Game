f30276902c535dc1782a8d8edcec6dadfacb68c7
Cliff Chen <56450369+yoonalove@users.noreply.github.com>
2020-03-17 14:46:53 -0400

    timer setup on canvas

--- js/app.js
+++ js/app.js
@@ -5,12 +5,18 @@ const app = {
     ctx: null,
     timerID: null,
     timer: 0,
+    positions: [],
 
+    initializePlayer() {
+        const player = new Player(400, 400, 8, 10);
+        this.players.push(player);
+        this.spawnUnit(player);
+    },
     startTimer() {
         this.timerID = setInterval(() => {
-            this.ctx.font = '30px serif';
+            
             this.timer++;
-            this.ctx.fillText(`Time to Daylight: ${this.timer}`, 700, 30)
+            
         }, 1000);
     },
     createCanvas() {
@@ -23,11 +29,40 @@ const app = {
         convas.height = 800;
         this.ctx = convas.getContext('2d');
     },
+    clearCanvas() {
+        app.ctx.clearRect(0, 0, 1000, 800);
+    },
     spawnUnit(unit) {
         this.ctx.beginPath();
-        this.ctx.arc(unit.x, unit.y, 10, 0, 2 * Math.PI);
+        this.ctx.arc(unit.x, unit.y, unit.radius, 0, 2 * Math.PI);
         this.ctx.fillStyle = unit.color;
         this.ctx.fill();
     },
-
+    animate() {
+        app.clearCanvas();
+        app.ctx.font = '30px serif';
+        app.ctx.fillText(`Time to Daylight: ${app.timer}`, 700, 30)
+        // for (player of this.players) {
+        //     this.spawnUnit(player);
+        // }
+        // for (zombie of this.zombies) {
+        //     this.spawnUnit(zombie);
+        // }
+        // for (citizen of this.citizens) {
+        //     this.spawnUnit(citizen);
+        // }
+        window.requestAnimationFrame(app.animate);
+    }
 }
+
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode == 39) {
+        console.log('right');
+    } else if (event.keyCode == 37) {
+        console.log('left');
+    } else if (event.keyCode == 38) {
+        console.log('up');
+    } else if (event.keyCode ==40 ) {
+        console.log('down');
+    }
+})
--- js/units.js
+++ js/units.js
@@ -1,28 +1,61 @@
 class Unit {
-    constructor(x, y, velocityX, velocityY) {
-        this.x = x;
-        this.y = y;
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
-    }
-    move() {
-        this.x += velocityX;
-        this.y += velocityY;
-    }
-    turnDirection(velocityX, velocityY) {
-        this.velocityX = velocityX;
-        this.velocityY = velocityY;
-    }
+  direction = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+  }
+  constructor(x, y, speed, radius) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.radius = radius;
+  }
+  move() {
+    if (this.direction.up) this.y -= this.speed;
+    if (this.direction.left) this.x -= this.speed;
+    if (this.direction.right) this.x += this.speed;
+    if (this.direction.down) this.y += this.speed;
+  }
+  setDirection(key) {
+    if(key == 'w') this.direction.up = true;
+    if(key == 'a') this.direction.left = true;
+    if(key == 's') this.direction.down = true;
+    if(key == 'd') this.direction.right = true;
+  }
+  unsetDirection(key) {
+    if(key == 'w') this.direction.up = false;
+    if(key == 'a') this.direction.left = false;
+    if(key == 's') this.direction.down = false;
+    if(key == 'd') this.direction.right = false;
+  }
+  checkCollision(unit) { // note this is designed to check for coll w/ other RECTs specifically
+    if(
+      this.x + this.radius > unit.x &&
+      this.x < unit.x + unit.radius && 
+      this.y + this.radius > unit.y &&
+      this.y < unit.y +unit.radius
+    ) {
+      console.log('collision');
+      return true
+    }  
+    else return false
+  }
 }
 
-class Citizen extends Unit{
-    color = 'white';
-    turnIntoZombie() {
-        // create a new zombie
-        const zombie = new Zombie(this.x, this.y, 0, 0);
-        return zombie;
-    }
+class Citizen extends Unit {
+  color = 'white';
+  turnIntoZombie() {
+    // create a new zombie
+    const zombie = new Zombie(this.x, this.y, 5, 10);
+    return zombie;
+  }
 }
-class Zombie extends Unit{
-    color = 'red';
+
+class Zombie extends Unit {
+  color = 'red';
+}
+
+class Player extends Unit {
+  color = 'yellow'
 }
